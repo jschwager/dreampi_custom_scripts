3a6199ec7e4e9ea2cbb5f059101a02f976e3c709
Jared Schwager <jared.schwager@gmail.com>
2026-04-18 20:11:02 -0400

    Added header and modified language/identifiers from Portugese to English

--- dcnet_on_off.sh
+++ dcnet_on_off.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+# Script name: DCNET ON/OFF Script for DreamPi
+# Original author: scrivanidc
+# Updated and maintained by: jschwager (onlycodered)
 
-#script by scrivanidc 20250124 #update 20250915
+# Updated 2025-09-15
 ACTION=$1
 
 if [ "$ACTION" = "enable" ]; then
-  opcao="1"
+  option="1"
 elif [ "$ACTION" = "disable" ]; then
-  opcao="2"
+  option="2"
 else
   echo "DCNET Server ON/OFF Script Switch"
   echo ""
@@ -14,12 +17,12 @@ else
   echo "1. DCNET Script ON"
   echo "2. DCNET Script OFF (Standard DreamPi)"
   echo "3. Delete DCNET Files"
-  read -p "Type 1, 2 or 3 and press Enter: " opcao
+  read -p "Type 1, 2 or 3 and press Enter: " option
 fi
 
 cd /home/pi/dreampi/
 echo ""
-verificar_arquivo_a() {
+check_dcnet_files() {
   if [ ! -f "dreampi_dcnet.py" ]; then
     echo "dreampi_dcnet.py backup and dcnet.rpi does not exist. Downloading..."
     echo ">>"
@@ -33,7 +36,7 @@ verificar_arquivo_a() {
   fi
 }
 
-verificar_arquivo_b() {
+check_standard_backup() {
   if [ ! -f "dreampi_standard.py" ]; then
     echo "dreampi_standard.py backup does not exist. Creating..."
     cp dreampi.py dreampi_standard.py
@@ -44,37 +47,37 @@ verificar_arquivo_b() {
   fi
 }
 
-copiar_arquivo_a() {
+copy_dcnet_script() {
   cp dreampi_dcnet.py dreampi.py
   echo "dreampi_dcnet.py copied to dreampi.py"
   echo ">>"
 }
 
-copiar_arquivo_b() {
+copy_standard_script() {
   cp dreampi_standard.py dreampi.py
   echo "dreampi_standard.py copied to dreampi.py"
   echo ">>"
 }
 
-deletar_arquivo_a() {
-  copiar_arquivo_b
+delete_dcnet_files() {
+  copy_standard_script
   rm dreampi_dcnet.py dcnet.rpi
-  echo "deleting DCNET files: dreampi_dcnet.py and dcnet.rpi"
+  echo "Deleting DCNET files: dreampi_dcnet.py and dcnet.rpi"
   echo "You're now able to download DCNET updated files at option 1"
   echo ">>"
 }
 
-verificar_arquivo_b
-verificar_arquivo_a
+check_standard_backup
+check_dcnet_files
 
-if [ "$opcao" -eq 1 ]; then
-  copiar_arquivo_a
+if [ "$option" -eq 1 ]; then
+  copy_dcnet_script
   echo "Done. DCNET Script ON (Standard DreamPi Script disabled)"
-elif [ "$opcao" -eq 2 ]; then
-  copiar_arquivo_b
+elif [ "$option" -eq 2 ]; then
+  copy_standard_script
   echo "Done. DCNET Script OFF (Standard DreamPi Script enabled)"
-elif [ "$opcao" -eq 3 ]; then
-  deletar_arquivo_a 
+elif [ "$option" -eq 3 ]; then
+  delete_dcnet_files 
   echo "Done. DCNET files deleted"
 else
   echo "Invalid option. Please choose 1, 2 or 3."
